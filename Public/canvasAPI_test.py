46b39918912235a9261551d03f9584ac9428da42
akahlils <73032120+akahlils@users.noreply.github.com>
2020-11-07 22:32:59 -0500

    Trying to fix Python

--- Public/canvasAPI_test.py
+++ Public/canvasAPI_test.py
@@ -1,6 +1,9 @@
 # https://canvasapi.readthedocs.io/en/latest/getting-started.html
 
- # -- import canvas
+# -- install package
+#!pip install canvasapi
+
+# -- import canvas
 from canvasapi import Canvas
 # -- UDCanvas API URL
 API_URL = 'https://canvas.instructure.com/'
@@ -12,30 +15,53 @@ canvas = Canvas(API_URL, API_KEY)
 # GET: https://canvas.instructure.com/api/v1/courses?access_token=<access_token>
 courses = canvas.get_courses()
 # -- format: <course_code> <course_name> <enrollment_state> <course_id>
+
+
+#Generate HTML file with courses
+
+print("Now Opening File:")
 f=open("coursesFound.html","w+")
 f.close
-for course in courses:    
-    print(course)
+
+print("Now generating file:")
+for course in courses:
+    f=open("coursesFound.html","a+")
+    f.write(str(course))
+    f.write("\n")
+    f.close
+
+print("Printing Text File:")
+t=open("coursesFound.html","r")
+print(t.read())
+
+
+
+
+
+
+
+#for course in courses:    
+  #  print(course)
     #Generate File
-    #print("Now Generating File")
-    #f=open("coursesFound.txt", "a")
+  #  print("Now Generating File")
+  #  f=open("coursesFound.html", "a")
     #for i in range(500):
-    #f.write(str(course))
-    #f.write("\n")
-    #f.close
-    #print("Text File:")
-    #t=open("coursesFound.html", "r")
-    #print(t.read())
+  #  f.write(str(course))
+  #  f.write("\n")
+  #  f.close
+ #   print("Text File:")
+ #   t=open("coursesFound.html", "r")
+ #   print(t.read())
 #course="Test Text"
 #Generate File
-    print("Now Generating File")
-    f=open("coursesFound.txt", "w+")
+   # print("Now Generating File")
+  #  f=open("coursesFound.txt", "w+")
 #for i in range(500):
-    f.write(course)
-    f.close
-print("Text File:")
-t=open("coursesFound.txt", "r")
-print(t.read())
+   # f.write(course)
+  #  f.close
+#print("Text File:")
+#t=open("coursesFound.txt", "r")
+#print(t.read())
 
 
 
